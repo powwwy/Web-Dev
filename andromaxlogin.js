4040656f006b6d6f549492b6d36a7abc91a6cc4d
ronnydracar <137864362+ronnydracar@users.noreply.github.com>
2023-07-21 19:39:10 +0400

    Update andromaxlogin.js

--- andromaxlogin.js
+++ andromaxlogin.js
@@ -1,15 +1,19 @@
-document.addEventListener("DOMContentLoaded", ( ) => {
+document.addEventListener("DOMContentLoaded", () => {
     const loginForm = document.querySelector("#login");
     const createAccountForm = document.querySelector("#createAccount");
-
-    document.querySelector("#linkCreateAccount") .addEventListener("click" , e =>{
-        e.preventDefault( );
-        loginForm.classList.add("form--hidden");
-        createAccountForm.classList.remove("form--hidden");
-    } ) ;
-
-    document.querySelector("#linkLogin") .addEventListener("click" , e =>{
-    loginForm.classList.remove("form--hidden");
-    createAccountForm.classList.add("form--hidden");
-    } ) ;
-} ) ;
+    const formTitle = document.querySelector("#formTitle");
+  
+    document.querySelector("#linkCreateAccount").addEventListener("click", e => {
+      e.preventDefault();
+      loginForm.classList.add("form--hidden");
+      createAccountForm.classList.remove("form--hidden");
+      formTitle.textContent = "Sign up for a fun rock experience";
+    });
+  
+    document.querySelector("#linkLogin").addEventListener("click", e => {
+      e.preventDefault();
+      createAccountForm.classList.add("form--hidden");
+      loginForm.classList.remove("form--hidden");
+      formTitle.textContent = "Login for a fun rock experience";
+    });
+  });
